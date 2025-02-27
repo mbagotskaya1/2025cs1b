3417dbdd3db703b1b452d836e69ffc900b66b574
mbagotskaya1 <mbagotskaya0@saddleback.edu>
2025-02-27 04:43:04 +0000

    -

---
+++ pointers/p3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std; 
+
+int * getArray(int count){
+    int * arr = new int[count];
+    for (int i = 0; i < count; i++) {
+        cin >> arr[i];
+    }
+    return arr;
+}
+
+void printArray(int * a, int count) {
+    for(int i = 0; i < count ; i++) {
+        cout << *(a + i) << endl;
+    }
+}
+
+int main()
+{
+    int * arr = getArray(3);
+    printArray(arr, 3);
+}
